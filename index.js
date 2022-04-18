0dbee7f7d9da27bfbe2dc69d5745f6027dbf8cc8
ridhorp <ridhogoodevil@gmail.com>
2022-04-18 11:29:27 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+
+app.use((error, req, res, next) => {
+    res.status(500).json({
+        status: "fail",
+        "message": error.message
+    })
+})
+
+app.use((req, res, next) => {
+    res.status(404).json({
+        status: "fail",
+        message: "not found"
+    })
+})
+
+app.listen(3000, () => {
+    console.log('server is running')
+})
